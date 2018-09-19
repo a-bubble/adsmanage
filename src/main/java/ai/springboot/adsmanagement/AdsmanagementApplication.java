f2bc92f24e0c8ecd62f64d881d2a7b92cf8ad552
艾营 <923106629@qq.com>
2018-09-19 15:31:52 +0800

    新建项目

---
+++ src/main/java/ai/springboot/adsmanagement/AdsmanagementApplication.java
@@ -0,0 +1,12 @@
+package ai.springboot.adsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsmanagementApplication.class, args);
+    }
+}
